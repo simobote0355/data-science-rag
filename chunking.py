ba3f3f6d8cd2971caf00e46f962057b2b3864707
simobote0355 <simobotero7445@gmail.com>
2026-08-03 18:32:13 -0500

    feat: add chunking and tokenization logic

---
+++ chunking.py
@@ -0,0 +1,37 @@
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("intfloat/multilingual-e5-small")
+
+def get_tokens(texto):
+    tokens = tokenizer.encode(texto)
+    return len(tokens)
+
+def split_chunk(item, threshold=400):
+    if item['tokens'] <= threshold:
+        return [item]
+    
+    paragraphs = item['content'].split('\n')
+
+    sub_chunks = []
+    actual_paragraph = []
+    actual_tokens = 0
+    aux = 1
+
+    for paragraph in paragraphs:
+        paragraph_tokens = get_tokens(paragraph)
+
+        if actual_tokens + paragraph_tokens > threshold and actual_paragraph:
+            content = '\n'.join(actual_paragraph)
+            sub_chunks.append({'title': f"{item['title']} (parte {aux})", 'content': content, 'source': item['source'], 'tokens': actual_tokens})
+            aux += 1
+            actual_paragraph = []
+            actual_tokens = 0
+
+        actual_paragraph.append(paragraph)
+        actual_tokens += paragraph_tokens
+        
+    if actual_paragraph:
+        content = '\n'.join(actual_paragraph)
+        sub_chunks.append({'title': f"{item['title']} (parte {aux})", 'content': content, 'source': item['source'], 'tokens': actual_tokens})
+
+    return sub_chunks
